907f0d524d3a6c67287adbf3d228998a2418551b
Bartosz Legięć <bartosz@legiec.eu>
2017-12-31 00:56:25 +0100

    Prepared to deploy & removed build/ from dev

--- server/passwords.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include '../../passwords.php';
-
-  // define("HOST", "localhost");
-  // define("LOGIN", "root");
-  // define("PASSWORD", "admin123");
-  // define("DATABASE", "quotes");
-?>

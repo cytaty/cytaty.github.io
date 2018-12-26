b12f1d51a8a9e31701de2044c053895b47ab0c25
Bartosz Legięć <bartosz.legiec@netguru.co>
2018-12-26 22:52:41 +0100

    Add basic php, mysql, apache setup

--- src/passwords.php
+++ src/passwords.php
@@ -1,9 +1,7 @@
 <?php
-  // include '../../passwords.php';
-
   define("HOST", getenv("DB_HOST"));
   define("LOGIN", getenv("DB_LOGIN"));
-  define("PASSWORD", getenv("DB_PASSWORD"));
-  define("DATABASE", getenv("DB_DATABASE"));
+  define("PASSWORD", getenv("MYSQL_ROOT_PASSWORD"));
+  define("DATABASE", getenv("MYSQL_DATABASE"));
   define("QUOTES_TABLE", getenv("DB_QUOTES_TABLE"));
 ?>

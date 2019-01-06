3cf98029cc6982519d3058f80e1567b216be8a5b
Bartosz Legięć <bartosz.legiec@netguru.co>
2019-01-06 23:14:05 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-REPOURL=$( echo $npm_package_repository_url | cut -d '+' -f 2 )
+
+REPOURL=$(echo $npm_package_repository_url | cut -d '+' -f 2)
 REPOBRANCH=$npm_package_repository_ghbranch
 
 if [ $REPOURL ] && [ $REPOBRANCH ]
@@ -7,11 +8,12 @@ then
   cp -r ./build ./.publish
   cd .publish
   git init
+  git remote add origin git@github.com:cytaty/cytaty.github.io.git
   git checkout -b $REPOBRANCH
   git add .
   git commit -m "Update $(date +"%FT%XZ")"
   git status
-  git push $REPOURL $REPOBRANCH -f
+  git push origin $REPOBRANCH -f
   cd ..
   rm -rf .publish
 
